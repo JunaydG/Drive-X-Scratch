9e14ae796f190244d4d3ec0aa032208614ba7857
Junayd Guelmani <junayd.guelmani18@gmail.com>
2022-10-05 18:52:44 +0200

    first commit

---
+++ drivers/drivers.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TP</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <?php
+    require_once "home/header.php";
+    require_once "./drivers/drivers-content.php";
+    require_once "./home/footer.php";
+    ?>
+</body>
+
+</html>
